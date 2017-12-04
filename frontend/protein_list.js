addf96c1eeef1216ab610906ad947bfb4922d278
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-12-03 23:28:07 -0800

    Add default proteins

--- frontend/protein_list.js
+++ frontend/protein_list.js
@@ -2,7 +2,13 @@ const defaults = [
   'P01096',
   'P0AG67',
   'P04637',
-  'P02549'
+  'P02549',
+  'P12345',
+  'P68250',
+  'P22629',
+  'P01034',
+  'P02647',
+  'P0AES6'
 ];
 
 export default defaults;
