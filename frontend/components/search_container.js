633b2b14eef83ff8463733802dbea1f62dd10b2b
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-12-10 12:24:38 -0800

    Create search container

---
+++ frontend/components/search_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import Search from './search';
+import { selectProteins, selectProteinIds } from '../reducers/selectors';
+import { requestProtein, receiveProtein } from '../actions/protein_actions';
+
+const mapStateToProps = state => ({
+  proteins: state.proteins,
+  proteinIds: selectProteinIds(state)
+})
+
+const mapDispatchToProps = dispatch => ({
+  requestProtein: (id) => dispatch(requestProtein(id)),
+  receiveProtein: (protein) => dispatch(receiveProtein(protein))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
