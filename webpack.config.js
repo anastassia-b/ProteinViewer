ab716742e8dab3e737fa51652251f960d22db53d
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-20 00:13:14 -0800

    Update to babel-env, configure jest and testing environment

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,14 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
         }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: 'source-map'
 };
